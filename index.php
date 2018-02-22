45de7c267cee7edfc448c3e35ff11e68d62778e7
Alessandrofpl <alessandrofpl@gmail.com>
2018-02-22 20:06:01 -0300

    01

---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="mit" content="2017-12-30T20:51:48-02:00+163182">
+        <title>Minha Clinica</title>
+        <meta name="description" content="Está página web fornece informações sobre a empresa Minha Clínica !"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <link rel="shortcut icon" href="img/logoicon.png"/>
+        <link rel="stylesheet" href="style.css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <p class="j_back backtop">Topo</p>
+
+        <header class="main_header container home">
+            <div class="content">
+                <img class="main_logo" title="Minha Clínica" alt="[Minha Clínica]" src="img/logoicon.png"/>
+                <h1 class="fontzero">Minha Clínica - Profissionais Especializados Sempre a Disposição</h1>
+
+                <ul class="main_menu">
+                    <li><a href="#home" title="Minha Clínica">Home</a></li>
+                    <li><a href="#corpo" title="Sobre a Empresa Minha Clínica">A Clínica</a></li>
+                    <li><a href="#especialidades" title="Especialidades">Especialidades</a></li>
+                    <li><a href="#clinico" title="Corpo Clínico">Corpo Clínico</a></li>
+                    <li><a href="#noticias" title="Notícias sobre a Minha Clínica ">Notícias</a></li>
+                    <li><a href="#contato" title="Contatos da Clínica">Contatos</a></li>
+                    <li> <img  title="Minha Clínica" alt="[Minha Clínica]" src="img/cellicon.png"/></li>
+		    <li><a class="special" href="#numero" title="Número">13 3333.CLIQUE</a></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <!--FECHA CABEÇALHO-->
+
+        <main class="container">
+            <div id="captioned-gallery">
+		<figure class="slider">
+		    <figure>
+			<img src="img/Rectangle 1.png" alt>
+			<figcaption>
+			    <p class="tagline">Clínica Especializada em</p>
+			    <h1>MEDICINA ESPORTIVA</h1>
+                            <p class="tagline">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+			</figcaption>
+		    </figure>
+		    <figure>
+			<img src="img/Rectangle 2.png" alt>
+			<figcaption>
+			    <p class="tagline">Clínica Especializada em</p>
+			    <h1>MEDICINA ESPORTIVA</h1>
+                            <p class="tagline">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+			</figcaption>
+		    </figure>
+		    <figure>
+			<img src="img/Rectangle 3.png" alt>
+			<figcaption>
+			    <p class="tagline">Clínica Especializada em</p>
+			    <h1>MEDICINA ESPORTIVA</h1>
+                            <p class="tagline">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+			</figcaption>
+		    </figure>
+		    <figure>
+			<img src="img/Rectangle 2.png" alt>
+			<figcaption>
+			    <p class="tagline">Clínica Especializada em</p>
+			    <h1>MEDICINA ESPORTIVA</h1>
+                            <p class="tagline">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+			</figcaption>
+		    </figure>
+		    <figure>
+			<img src="img/Rectangle 1.png" alt>
+			<figcaption>
+			    <p class="tagline">Clínica Especializada em</p>
+			    <h1>MEDICINA ESPORTIVA</h1>
+                            <p class="tagline">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+			</figcaption>
+		    </figure>
+		</figure>
+	    </div>
+            <section class="container fundo2">
+		<div class="content">
+                    <header class="al_center">
+                        <h3>Seja Bem-Vindo</h3>
+                        <h1 class="c_blue">CONHEÇA A CLÍNICA </h1>
+			<p  class="tagline">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p><br>
+                    </header>
+
+                    <article class="item_item al_center">
+                        <img src="img/Shape 4.png"/>
+                        <h1>Lorem ipsum dolor</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                    </article>
+
+                    <article class="item_item al_center">
+                        <img src="img/Shape 3.png"/>
+                        <h1>Lorem ipsum dolor</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                    </article>
+
+                    <article class="item_item al_center">
+                        <img src="img/Shape 2.png"/>
+                        <h1>Lorem ipsum dolor</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                    </article>
+
+                    <article class="item_item al_center">
+                        <img src="img/Shape 1.png"/>
+                        <h1>Lorem ipsum dolor</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                    </article>
+		</div>
+		<div class="clear"></div>
+
+
+                <footer class="container">
+		    <figure2 class="slider2">
+			<figure2>
+			    <img src="img/1.png" alt>
+			</figure2>
+			<figure2>
+			    <img src="img/2.png" alt>
+			</figure2>
+			<figure2>
+			    <img src="img/3.png" alt>
+			</figure2>
+			<figure2>
+			    <img src="img/4.png" alt>
+			</figure2>
+			<figure2>
+			    <img src="img/5.png" alt>
+			</figure2>
+		    </figure2>
+                </footer>
+
+            </section>
+
+            <section class="container fundo3 especialidades">
+                <div class="content">
+                    <header class="al_center c_white">
+                        Como podemos ajudar
+			<h1>Especialidades</h1>
+                    </header>
+		    
+                    <div class="main_menu2">
+                        <ul class="al_center">
+                            <li class="espaco"><img src="img/Forma3.png" alt><a href="#ortopedia" title="Ortopedia">Ortopedia</a></li>
+                            <li class="espaco"><img src="img/Forma 1.png" alt><a href="#fisioterapia" title="Fisioterapia">Fisioterapia</a></li>
+                            <li class="espaco"><img src="img/forma2.png" alt><a href="#medicinaesportiva" title="Medicina Esportiva">Medicina Esportiva</a></li>
+			</ul>
+                    </div>
+		    <div class="border_bottom">
+			<div class="content c_white">
+			    <hr><br/>
+			    <h1>Fisioterapia</h1>
+			    <p class="fisio" id="fisioterapia"> <img src="img/fisio.jpg"/></p>
+			    <div class="conteudo">
+                                <br><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
+
+				<ul>
+				    <li>Lorem ipsum dolor</li>
+				    <li>Lorem ipsum dolor</li>
+				    <li>Lorem ipsum dolor</li>
+				    <li>Lorem ipsum dolor</li>
+				    <li>Lorem ipsum dolor</li>
+				</ul>
+                                <div class="clear"></div>
+                            </div>
+			</div>
+		    </div>
+		    <div class="clear"></div>
+                </div>
+            </section>
+
+
+
+            <section class="conteiner">
+		<div class="content al_center">
+		    <header>
+                        <p class="al_corpo">Nossos Especialistas</p>
+			<h1 class="al_corpo2 titles_blue">Corpo Clínico</h1>
+                    </header>
+		    <img class="img_corpo" src="img/corpo.png">
+		    
+		    <div class="conteudo_corpo">
+			<h3 class="al_left titles_blue">Dr. Lorem ipsum</h3>
+		    <p class="al_left corpo_blue">Fisioterapeuta | CRM: 00.000</p> 
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+			<div class="setas">
+			    <a class="seta esq"><</a> <a class="seta dir">></a>
+			</div>
+		    </div>
+		</div>
+	    </section>
+
+	    <section class="container agenda noticias">
+		<div class="content img_agenda al_center">
+		    Saiba Mais
+                    <header class="titles_blue">
+                        <h1>Notícias</h1>
+                    </header>
+		    <img src="img/evento3.png">
+		    <img src="img/evento2.png">
+		    <img src="img/evento1.png">
+		    <div al_center>
+			<a class="btn_noticias">Mais Notícias</a>
+		    </div>
+		</div>
+	    </section>
+	    <article class="container contato forms">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3645.9080648002137!2d-46.3194705!3d-23.9636909!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce03a8c3d17c47%3A0x4e05949ab107867d!2sHospital+Guilherme+%C3%81lvaro!5e0!3m2!1spt-BR!2sbr!4v1517145158779" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <div class="content">
+		    <form class="contatei contato_form j_formsubmit" action="" method="post">
+			<header><h1>AGENDAR CONSULTA</h1></header>
+                        <input type="text" name="nome" title="Nome" placeholder="Nome:" required/>
+			<input type="tel" name="telefone" title="Telefone" placeholder="Telefone:" required/>
+			<input type="email" name="email" title="Informe Seu E-mail!" placeholder="E-mail:" required/>
+			<input type="text" name="Motivo" title="Motivo da Consulta" placeholder="Motivo da Consulta" required/>
+			<input type="date" name="date" title="Data" placeholder="Data" required/>
+			<input type="text" name="date" title="Data" placeholder="Periodo" required/>
+			<textarea rows="3" name="mensagem" title="Deixe sua Mensagem!" placeholder="Mensagem (Opcional)" required=""></textarea>
+			<button class="btn">Enviar</button>
+		    </form>
+	    </article>
+	    <div class="clear"></div>
+	</div>
+
+    </main>
+    <footer class="container main_footer1 al_center">
+	<div>
+	    <div class="ftcontent">
+		<p class="item_item al_right"><img src="img/footer_ico3.png"> AV. LOREM IPSUM DOLOR, 333 GONZAGA - SANTOS - SP</p>
+		<p class="item_item al_right"><img src="img/footer_ico2.png"> 13 3333.CLIQUE</p>
+		<p class="item_item al_right"><img src="img/footer_ico1.png"> ESTACIONAMENTO NO LOCAL</p>
+
+
+		<div class="clear"></div>
+	    </div>
+	</div>
+    </footer>
+
+    <footer class="al_center">
+
+	<div class="ftcontent2">
+	    <p class="item_item al_right">  Todos os direitos reservados - 2018</p>
+	    <p class="item_item al_right"> <img src="img/logoicon.png"></p>
+	    <p class="item_item al_right">  <img src="img/summer_logo_novo.png"></p>
+	</div>
+	<div class="clear"></div>
+
+
+    </footer>
+    <!--FECHA RODAPÉ-->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
